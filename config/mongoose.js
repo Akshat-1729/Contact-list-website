54766a560dd8d5ce00dd0ccd737c67fa3dd30562
Akshat-1729 <98538556+Akshat-1729@users.noreply.github.com>
2023-06-03 22:37:28 +0530

    config

---
+++ config/mongoose.js
@@ -0,0 +1,12 @@
+//require the library
+const mongoose=require('mongoose');
+//connect to database
+mongoose.connect('mongodb://127.0.0.1/test');
+//to check if tis successfull
+const db=mongoose.connection;
+//error
+db.on('error',console.error.bind(console,'error connecting to db'));
+//running
+db.once('open',function(){
+    console.log("successfully connected to DB");
+})
