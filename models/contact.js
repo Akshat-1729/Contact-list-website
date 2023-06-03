03c008305f0158ce76f7e6207024204da1174c9d
Akshat-1729 <98538556+Akshat-1729@users.noreply.github.com>
2023-06-03 22:36:54 +0530

    models

---
+++ models/contact.js
@@ -0,0 +1,13 @@
+const mongoose=require('mongoose');
+const ContactSchema=new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    phone:{
+        type:String,
+        required:true
+    }
+})
+const Contact=mongoose.model('contact',ContactSchema);
+module.exports=Contact;
